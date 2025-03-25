f8b80294a98b53ae8d2b1892e703da80846d373b
Aditya Pandey <146055820+adiayc@users.noreply.github.com>
2025-03-25 13:21:45 +0530

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,8 @@
 local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
 vim.cmd.source(vimrc)
 
+require('telescope.actions').select_default:replace(require('telescope.actions').select_tab)
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -42,21 +44,34 @@ require('lualine').setup {
   extensions = {}
 }
 
-require'tabline'.setup {
-      enable = true,
-      options = {
-        section_separators = {'', ''},
-        component_separators = {'', ''},
-        max_bufferline_percent = 66, 
-        show_tabs_always = false, 
-        show_devicons = true, 
-        show_bufnr = false, 
-        show_filename_only = false, 
-        modified_icon = "+ ", 
-        modified_italic = false, 
-        show_tabs_only = false, 
-      }
+require("toggleterm").setup()
+
+require('Comment').setup({ignore = '^$',
+  toggler = {
+    line = '<leader>cc',
+    block = '<leader>bc',
+  },
+  opleader = {
+    line = '<leader>c',
+    block = '<leader>b',
+  },
+})
+require('telescope').setup{
+  defaults = {
+    file_ignore_patterns = { ".git/" },  -- Still ignore .git directory
+    find_command = { "fd", "--type", "f", "--hidden", "--follow" }  -- Uses fd to search
+  },
+  pickers = {
+    find_files = {
+      hidden = true
+    },
+    live_grep = {
+      additional_args = function()
+        return { "--hidden" }
+      end
     }
+  }
+}
 
 local configs  = require'nvim-treesitter.configs'
 configs.setup{
@@ -68,5 +83,14 @@ configs.setup{
 	}
 }
 
-vim.cmd("colorscheme kanagawa")
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+vim.cmd("colorscheme tokyonight")
+
+vim.opt.tabstop    = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab  = true
--- nvim/vimrc.vim
+++ nvim/vimrc.vim
@@ -1,17 +1,41 @@
 call plug#begin(has('nvim') ? stdpath('data') . '/plugged' : '~/vim.plugged')
-
+Plug 'ms-jpq/coq_nvim', {'branch': 'coq'}
+Plug 'ms-jpq/coq.artifacts', {'branch': 'artifacts'}
+Plug 'ms-jpq/coq.thirdparty', {'branch': '3p'}
 Plug 'xiyaowong/transparent.nvim'
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'nvim-tree/nvim-web-devicons'
-Plug 'kdheepak/tabline.nvim'
 Plug 'sitiom/nvim-numbertoggle'
 Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}	
 Plug 'numToStr/Comment.nvim'
-Plug 'https://github.com/rebelot/kanagawa.nvim'
 Plug 'neovim/nvim-lspconfig'
+Plug 'folke/tokyonight.nvim'
+Plug 'akinsho/toggleterm.nvim', {'tag' : '*'}
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim', {'branch' : '1.0.x'}
+Plug 'tiagovla/scope.nvim' 
+Plug 'mg979/vim-visual-multi', {'branch': 'master'}
 
 call plug#end()
 
-lua require('Comment').setup()
+lua << EOF 
+require('Comment').setup({ignore = '^$',
+            toggler = {
+                line = '<leader>cc',
+                block = '<leader>bc',
+            },
+            opleader = {
+                line = '<leader>c',
+                block = '<leader>b',
+            },
+        })
+EOF
+
+
+set tabstop=2  
+set shiftwidth=2
+set expandtab
+set softtabstop=2
+
 set number
 let g:loaded_node_provider = 0
